b88a9d62334541d162cfdc46526b0538b0f30184
kshntn <kshntn@gmail.com>
2018-06-02 15:59:24 +0200

    new project

---
+++ Building BlackJack Counter/BinaryThreshold.py
@@ -0,0 +1,10 @@
+import cv2
+
+img1= cv2.imread('contours.png',0)	# converts to greyscale
+img2=cv2.imread('example.jpg',0)
+
+
+_,thresh1=cv2.threshold(img2,220,255,cv2.THRESH_BINARY)
+cv2.imshow('thresh1',thresh1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
